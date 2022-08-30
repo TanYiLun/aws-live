cadd73da74496147fbcc20c2958bb6ecaa292077
TanYiLun <78059048+TanYiLun@users.noreply.github.com>
2022-08-30 14:19:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cfw74uzksic0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanyilun-employee"
+customregion = "us-east-1"
