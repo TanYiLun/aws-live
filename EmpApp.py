cd3a15b5f2ed21f3a532eebd8e94165c0ca22936
Yung How <81214607+TeeYH12345@users.noreply.github.com>
2022-09-21 16:21:15 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -21,15 +21,77 @@ output = {}
 table = 'employee'
 
 
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('LoginPage.html')
 
 
 @app.route("/about", methods=['POST'])
 def about():
     return render_template('www.intellipaat.com')
 
+@app.route("/RegisterPageRoute")
+def toRegisterPage:
+    return render_template('RegisterPage.html')
+
+@app.route("/Register")
+def registerAccount:
+    user_id = lower(request.form['user_id'])
+    user_password = request.form['user_password']
+    user_confirm_password = request.form["user_confirm_password"]
+
+    insert_sql = "INSERT INTO user VALUES (%s, %s)"
+    check_sql = "SELECT COUNT(user_id) FROM user WHERE user_id=(%s)"
+    cursor = db_conn.cursor()
+
+    if user_confirm_password!=user_password:
+        return "Password does not match with confirm password"
+
+    if (cursor.execute(check_sql, (user_id)))!=0
+        return "User Id already exist"
+
+    try:
+
+        cursor.execute(insert_sql, (user_id, user_password))
+        db_conn.commit()
+
+    finally:
+        cursor.close()
+
+    print("Successfully registered, redirecting to login page")
+    return render_template("LoginPage.html")
+
+@app.route("/Login")
+def registerAccount:
+    user_id = lower(request.form['user_id'])
+    user_password = request.form['user_password']
+"
+    check_id = "SELECT COUNT(user_id) FROM user WHERE user_id=(%s)"
+    check_pw = "SELECT COUNT(user_password) FROM user WHERE user_password=(%s)"
+    correct_id = False
+    correct_pw = False
+    cursor = db_conn.cursor()
+
+    if (cursor.execute(check_id, (user_id)))>0
+        correct_id = True
+
+    if (cursor.execute(check_pw, (user_password)))>0
+        correct_pw = True
+   
+    if correct_id and correct_pw:
+        print("Login successful")
+        return render_template('AddEmp.html')
+    else:
+        print("Invalid user id or/and password")
+        correct_id = False
+        correct_pw = False
+        return render_template('LoginPage.html')
+
+
+@app.route("/LoginPageRoute")
+def toLoginPage:
+    return render_template('LoginPage.html')
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
