96a69def81a4931df1654b9520c0cfaa5894c7af
TanYiLun <78059048+TanYiLun@users.noreply.github.com>
2022-09-28 00:20:50 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -234,7 +234,7 @@ def AddEmp():
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.client('s3')
+        s3 = boto3.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
