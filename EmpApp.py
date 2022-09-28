74937b250428031a4dc171c00fc46c139496e8cd
TheOje <82089034+TheOje@users.noreply.github.com>
2022-09-28 18:20:03 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -186,7 +186,7 @@ def checkOut():
         checkOutTime = datetime.now()
         LoginDate = datetime.strptime(formatted_login[0],'%Y-%m-%d %H:%M:%S' )
 
-        formatted_checkout = checkOutTime.strptime(formatted_login[0],'%Y-%m-%d %H:%M:%S' )
+        formatted_checkout = checkOutTime.strptime(formatted_checkout[0],'%Y-%m-%d %H:%M:%S' )
 
         try:
            cursor.execute(insert_statement,(emp_id,formatted_login[0],formatted_checkout))
