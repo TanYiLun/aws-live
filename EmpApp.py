e639d94bc5d547ca393bf43f0de9e6e900cbc078
Yung How <81214607+TeeYH12345@users.noreply.github.com>
2022-09-28 19:13:25 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -112,14 +112,13 @@ def LoginUser():
 
     check_id = "SELECT * FROM user WHERE user_id=(%s)"
     check_pw = "SELECT * FROM user WHERE user_password=(%s)"
+    cursor = db_conn.cursor()
     cursor.execute(check_id, (user_id))
     id_sql = str(cursor.fetchall())
-    cursor = db_conn.cursor()
     cursor.execute(check_pw, (user_password))
     pw_sql = str(cursor.fetchall())
     correct_id = False
     correct_pw = False
-    cursor = db_conn.cursor()
     
     
     if id_sql !="()":
